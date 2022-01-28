59a2ce4d059bc45141d6c023cdd38589a33ccaed
VakuTheDaku <vinayarunkumarkp3@gmail.com>
2022-01-28 21:39:08 +0530

    Aarambam

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(Home());
+}
+class Home extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("Bucket_List_App pani thudangikolu :D", textDirection: TextDirection.ltr,),
+
+    );
+  }
+}
+
